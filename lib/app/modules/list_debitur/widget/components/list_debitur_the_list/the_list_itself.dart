ac199d7612b13459bfbf86cb66eb2202cb9a17c4
Novian Andika <novian.andika@students.amikom.ac.id>
2023-03-31 15:35:39 +0700

    🎨 style(the_list_itself.dart): refactor ListTile widget The code has been refactored to improve readability and maintainability. The ListTile widget has been modified to include a TextSubtitle widget that displays the subtitle of the ListTile. The color of the ListTile is now determined by whether the user ID matches the current user's ID. The code has been formatted to follow the Dart style guide.
    ✨ feat(the_list_itself.dart): add TextSubtitle widget
    This commit adds a new widget called TextSubtitle to the ListAllDebitur class. The TextSubtitle widget is responsible for displaying the subtitle of each item in the list of debitur. It contains a Table widget that displays various information about the debitur such as Tanggal Input, Domisili, Jenis Usaha, Bidang Usaha, Usia Debitur, Analis, and Plafond Kredit. It also displays the progress of the debitur in percentage using a LinearProgressIndicator widget.

--- lib/app/modules/list_debitur/widget/components/list_debitur_the_list/the_list_itself.dart
+++ lib/app/modules/list_debitur/widget/components/list_debitur_the_list/the_list_itself.dart
@@ -1,9 +1,11 @@
 // 🐦 Flutter imports:
+import 'package:akm/app/common/constant.dart';
 import 'package:akm/app/modules/list_debitur/controllers/list_debitur_controller.dart';
+import 'package:extended_masked_text/extended_masked_text.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 // 📦 Package imports:
-import 'package:intl/intl.dart';
 import 'package:random_avatar/random_avatar.dart';
 
 // 🌎 Project imports:
@@ -35,136 +37,240 @@ class ListAllDebitur extends StatelessWidget {
                   Radius.circular(30),
                 ),
               ),
-              color: Colors.blue[900]?.withOpacity(0.9),
+              color:
+                  controller.listDebitur[index].userId == auth.currentUser?.uid
+                      ? Colors.blue[900]?.withOpacity(0.9)
+                      : Colors.red[900]?.withOpacity(0.9),
               elevation: 6,
               child: ListTile(
-                  leading: RandomAvatar(
-                    '${controller.listDebitur[index].peminjam1}',
-                    fit: BoxFit.cover,
-                    height: 50,
-                    width: 50,
+                leading: RandomAvatar(
+                  '${controller.listDebitur[index].peminjam1}',
+                  fit: BoxFit.cover,
+                  height: 50,
+                  width: 50,
+                ),
+                enableFeedback: true,
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                title: Text(
+                  '${controller.listDebitur[index].peminjam1}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
-                  enableFeedback: true,
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 10,
-                  ),
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '${controller.listDebitur[index].peminjam1}',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        DateFormat('EEEE, dd MMMM yyyy', 'id_ID')
-                            .format(controller.listDebitur[index].tglSekarang!),
-                        style: const TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 5.0,
-                      ),
-                      Table(
-                        columnWidths: const {
-                          0: FlexColumnWidth(0.30),
-                          1: FlexColumnWidth(0.03),
-                          2: FlexColumnWidth(0.67),
-                        },
-                        children: [
-                          TableRow(
-                            children: [
-                              Text(
-                                'Domisili',
-                                style: subtitleStyle,
-                              ),
-                              Text(
-                                ':',
-                                style: subtitleStyle,
-                              ),
-                              Text(
-                                '${controller.listDebitur[index].ktp1}',
-                                style: subtitleStyle,
-                              ),
-                            ],
-                          ),
-                          TableRow(
-                            children: [
-                              Text(
-                                'Bidang Usaha',
-                                style: subtitleStyle,
-                              ),
-                              Text(
-                                ':',
-                                style: subtitleStyle,
-                              ),
-                              Text(
-                                '${controller.listDebitur[index].bidangUsaha}',
-                                style: subtitleStyle,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 10.0,
-                      ),
-                      Text(
-                        'Progress: ${(double.parse(controller.listDebitur[index].progress!) * 100).toStringAsFixed(0)} %',
-                        style: const TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 5.0,
-                      ),
-                      SizedBox(
-                        height: 10,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: LinearProgressIndicator(
-                            value: double.parse(
-                                controller.listDebitur[index].progress!),
-                            backgroundColor: Colors.grey[300],
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                              (double.parse(controller
-                                              .listDebitur[index].progress!) >=
-                                          0.1 &&
-                                      double.parse(controller
-                                              .listDebitur[index].progress!) <
-                                          0.6)
-                                  ? Colors.red
-                                  : (double.parse(controller.listDebitur[index]
-                                                  .progress!) >=
-                                              0.6 &&
-                                          double.parse(controller
-                                                  .listDebitur[index]
-                                                  .progress!) <
-                                              1.0)
-                                      ? Colors.yellow
-                                      : Colors.green,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 5.0,
-                      ),
-                    ],
-                  )),
+                ),
+                subtitle: TextSubtitle(
+                  subtitleStyle: subtitleStyle,
+                  controller: controller,
+                  index: index,
+                ),
+                onTap: () {},
+                splashColor: Colors.blue[900]?.withOpacity(0.4),
+              ),
             ),
           );
         },
       ),
+    );
+  }
+}
+
+class TextSubtitle extends StatelessWidget {
+  final int index;
+
+  const TextSubtitle({
+    super.key,
+    required this.subtitleStyle,
+    required this.controller,
+    required this.index,
+  });
+
+  final TextStyle subtitleStyle;
+  final ListDebiturController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 5.0,
+        ),
+        Table(
+          columnWidths: const {
+            0: FlexColumnWidth(0.30),
+            1: FlexColumnWidth(0.03),
+            2: FlexColumnWidth(0.67),
+          },
+          children: [
+            TableRow(
+              children: [
+                Text(
+                  'Tanggal Input',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  ':',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  DateFormat('EEEE, dd MMMM yyyy', 'id_ID')
+                      .format(controller.listDebitur[index].tglSekarang!),
+                  style: subtitleStyle,
+                ),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text(
+                  'Domisili',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  ':',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  '${controller.listDebitur[index].ktp1}',
+                  style: subtitleStyle,
+                ),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text(
+                  'Jenis Usaha',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  ':',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  '${controller.listDebitur[index].jenisUsaha}',
+                  style: subtitleStyle,
+                ),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text(
+                  'Bidang Usaha',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  ':',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  '${controller.listDebitur[index].bidangUsaha}',
+                  style: subtitleStyle,
+                ),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text(
+                  'Usia Debitur',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  ':',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  '${controller.listDebitur[index].umur} Tahun',
+                  style: subtitleStyle,
+                ),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text(
+                  'Analis',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  ':',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  '${controller.listDebitur[index].user?.displayName!}',
+                  style: subtitleStyle,
+                ),
+              ],
+            ),
+            TableRow(
+              children: [
+                Text(
+                  'Plafond Kredit',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  ':',
+                  style: subtitleStyle,
+                ),
+                Text(
+                  controller.listDebitur[index].inputKeuangan == null
+                      ? 'Belum Input'
+                      : MoneyMaskedTextController(
+                          decimalSeparator: '',
+                          thousandSeparator: '.',
+                          leftSymbol: 'Rp ',
+                          precision: 0,
+                          initialValue: double.parse(
+                            controller.listDebitur[index].inputKeuangan!
+                                .kreditDiusulkan
+                                .toString(),
+                          ),
+                        ).text,
+                  style: subtitleStyle,
+                ),
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          'Progress: ${(double.parse(controller.listDebitur[index].progress!) * 100).toStringAsFixed(0)} %',
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(
+          height: 5.0,
+        ),
+        SizedBox(
+          height: 10,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: LinearProgressIndicator(
+              value: double.parse(controller.listDebitur[index].progress!),
+              backgroundColor: Colors.grey[300],
+              valueColor: AlwaysStoppedAnimation<Color>(
+                (double.parse(controller.listDebitur[index].progress!) >= 0.1 &&
+                        double.parse(controller.listDebitur[index].progress!) <
+                            0.6)
+                    ? Colors.red
+                    : (double.parse(controller.listDebitur[index].progress!) >=
+                                0.6 &&
+                            double.parse(
+                                    controller.listDebitur[index].progress!) <
+                                1.0)
+                        ? Colors.yellow
+                        : Colors.green,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5.0,
+        ),
+      ],
     );
   }
 }

8b5e328257dc5f8571a979a93e85ab15ce719ebd
Novian Andika <novian.andika@students.amikom.ac.id>
2023-03-31 10:24:09 +0700

    🔥 feat: refactor the code to use the new API that is more flexible and easier to use (and more efficient)

---
+++ lib/app/modules/home/widget/components/home_profile/user_input_components/user_input_reuseable_textfield.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class TextInputProfile extends StatelessWidget {
+  final String name;
+  final TextEditingController controller;
+  final Widget? suffixIcon;
+  final IconData? prefixIcon;
+  final bool? readOnly;
+
+  const TextInputProfile({
+    super.key,
+    required this.name,
+    required this.controller,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.readOnly,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilderTextField(
+      name: name,
+      readOnly: readOnly ?? false,
+      controller: controller,
+      cursorColor: Colors.white,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.blue[900]?.withOpacity(0.9),
+        suffixIcon: suffixIcon,
+        prefixIcon: prefixIcon != null
+            ? Icon(
+                prefixIcon,
+                color: Colors.white,
+              )
+            : null,
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
+          ),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.transparent,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
+          ),
+        ),
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.transparent,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
+          ),
+        ),
+      ),
+    );
+  }
+}

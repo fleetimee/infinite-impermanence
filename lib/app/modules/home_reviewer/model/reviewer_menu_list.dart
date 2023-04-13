63ea89b0db2649355e97347706405c2b9f45ba94
Novian Andika <novian.andika@students.amikom.ac.id>
2023-04-13 14:53:10 +0700

    ✨ feat(reviewer_menu_list.dart, reviewer_buttons.dart): add reviewer menu list and buttons This commit adds a list of reviewer menu items and buttons to the reviewer home page. The menu items include Pengajuan Pending, Pengajuan Reviewed, Statistik & Data, and Keluar. The buttons are designed to be clickable and will navigate the user to the corresponding page. The menu items are displayed with their respective images, names, and descriptions.
    ✨ feat(reviewer_greetings.dart): add ReviewerGreeting widget
    This commit adds a new widget called ReviewerGreeting. It displays a greeting message, user account name, and profile image. The widget also includes a wave animation in the background and the current date in the top right corner. The ReviewerGreeting widget is used in the home screen of the application.
    
    ✨ feat(reviewer_information.dart): add ReviewerDeviceInformation widget
    This commit adds a new widget called ReviewerDeviceInformation to the home_reviewer_main_menu folder. The widget displays information about the device the reviewer is using, including the brand name, product name, address, and main and helper branches. The widget is used in the home_reviewer_main_menu.dart file.
    
    🔥 refactor(home_reviewer_main_menu.dart): remove unused imports and code
    This commit removes unused imports and code, including the Awesome Dialog package, the Font Awesome Flutter package, the Google Fonts package, the Cached Network Image package, the Wave package, and the WavesSettings class. It also removes the wave animation and replaces it with a static background color. The code for displaying the user's profile image and information is now encapsulated in a separate component called ReviewerGreeting. The SingleChildScrollView now has a NeverScrollableScrollPhysics to prevent scrolling.
    
    🔥 refactor(home_reviewer_main_menu.dart): remove unused code and widgets
    The code removed includes the following:
    - Three cards that were not being used
    - Padding and Row widgets that were wrapping the cards
    - SizedBox widgets that were used for spacing
    - InkWell widgets that were wrapping the cards
    - AwesomeDialog widget that was used for confirmation before exiting the app
    
    The ReviewerDeviceInformation widget was replaced with the ReviewerButton widget, which is a more appropriate widget for the main menu.

---
+++ lib/app/modules/home_reviewer/model/reviewer_menu_list.dart
@@ -0,0 +1,79 @@
+import 'package:akm/app/modules/home_reviewer/controllers/home_reviewer_controller.dart';
+import 'package:akm/app/routes/app_pages.dart';
+import 'package:akm/app/widget/dialog_box.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+class ReviewerListMenu {
+  final String menuName;
+  final String menuDescription;
+  final String imagePath;
+  final Function onTap;
+
+  ReviewerListMenu({
+    required this.menuName,
+    required this.menuDescription,
+    required this.imagePath,
+    required this.onTap,
+  });
+}
+
+final List<ReviewerListMenu> reviewerMenuList = [
+  ReviewerListMenu(
+    menuName: 'Pengajuan Pending',
+    menuDescription: 'Nilai Pengajuan dari Analis yang belum di review',
+    imagePath: 'assets/images/home/reviewer_pengajuan_pending.jpg',
+    onTap: () {
+      final controller = Get.find<HomeReviewerController>();
+
+      Get.toNamed(
+        Routes.REVIEWER_PENDING_LIST,
+        arguments: controller.uid,
+      );
+    },
+  ),
+  ReviewerListMenu(
+    menuName: 'Pengajuan Reviewed',
+    menuDescription: 'Kumpulan Pengajuan yang sudah di review',
+    imagePath: 'assets/images/home/reviewer_pengajuan_reviewed.jpg',
+    onTap: () {
+      final controller = Get.find<HomeReviewerController>();
+
+      Get.toNamed(
+        Routes.REVIEWER_COMPLETED_LIST,
+        arguments: controller.uid,
+      );
+    },
+  ),
+  ReviewerListMenu(
+    menuName: 'Statistik & Data',
+    menuDescription: 'Statistik dan Data mengenai pengajuan',
+    imagePath: 'assets/images/home/reviewer_stats.jpg',
+    onTap: () {
+      final controller = Get.find<HomeReviewerController>();
+
+      Get.toNamed(
+        Routes.REVIEWER_STATS,
+        arguments: controller.uid,
+      );
+    },
+  ),
+  ReviewerListMenu(
+    menuName: 'Keluar',
+    menuDescription: 'Keluar dari Aplikasi',
+    imagePath: 'assets/images/home/reviewer_keluar.jpg',
+    onTap: () {
+      PrompDialog(
+        title: 'Keluar',
+        context: Get.context!,
+        desc: 'Apakah anda yakin ingin keluar dari aplikasi?',
+        btnOkText: 'Ya',
+        btnCancelText: 'Tidak',
+        btnCancelOnPress: () {},
+        btnOkOnPress: () {
+          SystemNavigator.pop();
+        },
+      ).show();
+    },
+  ),
+];

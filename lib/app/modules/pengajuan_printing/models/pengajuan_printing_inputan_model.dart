5127d34eabcbb48fe003eb1c8f1e5ce220058292
Novian Andika <hello@fleetime.my.id>
2023-04-27 06:00:48 +0700

    feat: refactor

---
+++ lib/app/modules/pengajuan_printing/models/pengajuan_printing_inputan_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PrintCardPengajuanDetail {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final Function onTap;
+
+  PrintCardPengajuanDetail({
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    required this.onTap,
+  });
+}
--- lib/app/modules/pengajuan_printing/views/pengajuan_printing_view.dart
+++ lib/app/modules/pengajuan_printing/views/pengajuan_printing_view.dart
@@ -36,60 +36,9 @@ class PengajuanPrintingView extends GetView<PengajuanPrintingController> {
                       children: [
                         Column(
                           children: [
-                            Card(
-                              color: Colors.green[100],
-                              child: Container(
-                                width: MediaQuery.of(context).size.width,
-                                padding: const EdgeInsets.all(20.0),
-                                child: Row(
-                                  children: [
-                                    Expanded(
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          const Text(
-                                            "Print",
-                                            style: TextStyle(
-                                              fontSize: 12.0,
-                                            ),
-                                          ),
-                                          const SizedBox(
-                                            height: 6.0,
-                                          ),
-                                          Row(
-                                            children: const [
-                                              Text(
-                                                "Inputan",
-                                                style: TextStyle(
-                                                  fontSize: 16.0,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    Container(
-                                      padding: const EdgeInsets.all(10.0),
-                                      decoration: BoxDecoration(
-                                        color: Colors.green[400],
-                                        borderRadius: const BorderRadius.all(
-                                          Radius.circular(
-                                            8.0,
-                                          ),
-                                        ),
-                                      ),
-                                      child: const Icon(
-                                        Icons.edit_note,
-                                        size: 24.0,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
+                            const HeaderPrintingInputan(),
+                            const SizedBox(
+                              height: 5.0,
                             ),
                             Card(
                               child: ListTile(
@@ -739,6 +688,73 @@ class PengajuanPrintingView extends GetView<PengajuanPrintingController> {
                     ),
                   ),
                 ),
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderPrintingInputan extends StatelessWidget {
+  const HeaderPrintingInputan({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.green[100],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Print",
+                    style: TextStyle(
+                      fontSize: 12.0,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6.0,
+                  ),
+                  Row(
+                    children: const [
+                      Text(
+                        "Inputan",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.green[400],
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(
+                    8.0,
+                  ),
+                ),
+              ),
+              child: const Icon(
+                Icons.edit_note,
+                size: 24.0,
+                color: Colors.white,
+              ),
+            ),
+          ],
         ),
       ),
     );
